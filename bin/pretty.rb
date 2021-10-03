17a81f3462e732ae999bac208ae148e376463702
Yoshiyuki Koyanagi <moutend@gmail.com>
2021-10-03 20:37:50 +0900

    Add

---
+++ bin/pretty.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+ARGF.each do |line|
+  line.gsub!(/\[([^{]*)\]{.gfont.bold}/, '\1')
+  line.gsub!(/\[([^{]*)\]{.italic}/, '\1')
+  line.gsub!(/\[([^{]*)\]{.bold}/, '\1')
+  line.gsub!(/\[([^{]*)\]{.word}/, '`\1`')
+  line.gsub!(/\[([^{]*)\]{.wordn}/, '`\1`')
+  puts line
+end
