574594e279b4ce842511767b89601876a013375c
Yoshiyuki Koyanagi <moutend@gmail.com>
2021-11-05 18:07:00 +0900

    Update

--- bin/brew.bash
+++ bin/brew.bash
@@ -10,17 +10,8 @@ brew install pandoc
 brew install nkf
 brew install exiftool
 brew install ffmpeg
-brew install w3m
 brew install wget
-brew install gpg
-brew install postgresql
 
 brew tap laurent22/massren
 brew install massren
 massren --config editor vim
-
-brew tap moutend/homebrew-gip
-brew install gip
-
-brew tap moutend/homebrew-mediumctl
-brew install mediumctl
