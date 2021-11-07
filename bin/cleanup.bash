3b2c11a0b37f21b070df6c3452e1b6fad6d702d5
Yoshiyuki Koyanagi <moutend@gmail.com>
2021-11-07 11:25:05 +0900

    Update

--- bin/cleanup.bash
+++ bin/cleanup.bash
@@ -4,6 +4,7 @@ set -e
 
 rm ~/.c2d
 rm ~/.envrc
+rm ~/.gitignore
 rm ~/.gitconfig
 rm ~/.gitconfig.abby-develop
 rm ~/.gittemplate
--- bin/setup.bash
+++ bin/setup.bash
@@ -4,6 +4,7 @@ set -e
 
 ln -s ~/github.com/moutend/dotfiles/.c2d ~/.c2d
 ln -s ~/github.com/moutend/dotfiles/.envrc ~/.envrc
+ln -s ~/github.com/moutend/dotfiles/.gitignore ~/.gitignore
 ln -s ~/github.com/moutend/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/github.com/moutend/dotfiles/.gitconfig.abby-develop ~/.gitconfig.abby-develop
 ln -s ~/github.com/moutend/dotfiles/.gittemplate ~/.gittemplate
