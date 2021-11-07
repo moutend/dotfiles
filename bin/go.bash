29cd2d32ac363c570125ec129bbfffeead7562f2
Yoshiyuki Koyanagi <moutend@gmail.com>
2021-11-07 16:06:45 +0900

    Update

--- bin/go.bash
+++ bin/go.bash
@@ -8,3 +8,4 @@ go install github.com/moutend/cwd/cmd/cwd@latest
 go install github.com/moutend/c2d/cmd/c2d@latest
 go install github.com/moutend/xor/cmd/xor@latest
 go install github.com/moutend/slack/cmd/slack@latest
+go install github.com/moutend/rmdot/cmd/rmdot@latest
