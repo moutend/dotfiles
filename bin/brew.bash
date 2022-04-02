6ac13814c821b16114f5daba9d4c1887fab9cda4
Yoshiyuki Koyanagi <moutend@gmail.com>
2022-04-02 11:15:11 +0900

    Update

--- bin/brew.bash
+++ bin/brew.bash
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 brew install sox
-brew install curl
 brew install direnv
-brew install git-lfs
+brew install curl
 brew install jq
 brew install tree
 brew install pandoc
 brew install nkf
 brew install exiftool
 brew install ffmpeg
+brew install git-lfs
 brew install wget
+brew install blackhole-2ch
 
 brew tap laurent22/massren
 brew install massren
--- bin/go.bash
+++ bin/go.bash
@@ -2,13 +2,13 @@
 
 set -e
 
-go install golang.org/x/tools/cmd/goimports@latest
-go install github.com/moutend/codespeak/cmd/codespeak@latest
-go install github.com/moutend/cwd/cmd/cwd@latest
 go install github.com/moutend/c2d/cmd/c2d@latest
 go install github.com/moutend/xor/cmd/xor@latest
+go install github.com/moutend/codespeak/cmd/codespeak@latest
+go install github.com/moutend/cwd/cmd/cwd@latest
 go install github.com/moutend/slack/cmd/slack@latest
 go install github.com/moutend/rmdot/cmd/rmdot@latest
 go install github.com/moutend/backlogctl/cmd/backlogctl@latest
 go install github.com/moutend/web/cmd/web@latest
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.43.0
+go install golang.org/x/tools/cmd/goimports@latest
