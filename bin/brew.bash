b59af52ff45e3f47ff32564519359456c8877d6f
Yoshiyuki Koyanagi <moutend@gmail.com>
2023-01-08 21:35:53 +0900

    Update

--- bin/brew.bash
+++ bin/brew.bash
@@ -3,16 +3,18 @@
 brew install sox
 brew install direnv
 brew install curl
-brew install nkf
-brew install gsed
-brew install gawk
-brew install pandoc
-brew install jq
-brew install tree
-brew install exiftool
-brew install ffmpeg
-brew install git-lfs
-brew install blackhole-2ch
+
+# brew install nkf
+# brew install gsed
+# brew install gawk
+# brew install pandoc
+# brew install jq
+# brew install tree
+# brew install exiftool
+# brew install ffmpeg
+# brew install git-lfs
+# brew install youtube-dl
+# brew install blackhole-2ch
 
 brew tap laurent22/massren
 brew install massren
