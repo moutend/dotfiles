475d9d139af90a9685056541415a0f092bd02b25
Yoshiyuki Koyanagi <moutend@gmail.com>
2023-01-22 15:08:39 +0900

    Update

--- bin/png2mp4.bash
+++ bin/png2mp4.bash
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-ffmpeg -loop 1 -r 30000/1001 -i image.png -i audio.wav -vcodec libx264 -acodec aac -strict experimental -ab 320k -ac 2 -ar 48000 -pix_fmt yuv420p -shortest output.mp4
+ffmpeg \
+  -loop 1 -i image.png \
+  -i audio.wav \
+  -vcodec libx264 -acodec aac \
+  -ab 320k -ac 1 -ar 48000 \
+  -pix_fmt yuv420p -r 30 \ # 動画のフレームレートを30 fpsに設定
+  -t 6.0 \ # 動画の長さを設定
+  output.mp4
