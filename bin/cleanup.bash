41d652b007dde44e6dd556f863ab3a0d519725e6
Yoshiyuki Koyanagi <moutend@gmail.com>
2023-06-10 07:26:28 +0900

    Update

--- bin/cleanup.bash
+++ bin/cleanup.bash
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-set -e
+remove_symbolic_link() {
+  rm -rf "$HOME/$1"
+  echo "OK	remove_symbolic_link $1"
+}
 
-rm -rf ~/.pongbox
-rm -rf ~/.c2d
-rm -rf ~/.envrc
-rm -rf ~/.gitignore
-rm -rf ~/.gitconfig
-rm -rf ~/.gitconfig.abby-develop
-rm -rf ~/.gittemplate
-rm -rf ~/.profile
-rm -rf ~/.vimrc
-rm -rf ~/.zshrc
-
-rm -rf ~/.vimswap
+remove_symbolic_link '.profile'
+remove_symbolic_link '.zshrc'
+remove_symbolic_link '.vimrc'
+remove_symbolic_link '.c2d'
+remove_symbolic_link '.gitignore'
+remove_symbolic_link '.gitconfig'
+remove_symbolic_link '.gitconfig.abby-develop'
--- bin/setup.bash
+++ bin/setup.bash
@@ -1,16 +1,59 @@
 #!/bin/bash
 
-set -e
+create_home_bin_directory() {
+  if [ ! -d $HOME/bin ]; then
+    mkdir "$HOME/bin"
+    echo "OK	create_home_bin_directory"
+  else
+    echo "SKIP	create_home_bin_directory"
+  fi
+}
 
-ln -s ~/github.com/moutend/dotfiles/.pongbox ~/.pongbox
-ln -s ~/github.com/moutend/dotfiles/.c2d ~/.c2d
-ln -s ~/github.com/moutend/dotfiles/.envrc ~/.envrc
-ln -s ~/github.com/moutend/dotfiles/.gitignore ~/.gitignore
-ln -s ~/github.com/moutend/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/github.com/moutend/dotfiles/.gitconfig.abby-develop ~/.gitconfig.abby-develop
-ln -s ~/github.com/moutend/dotfiles/.gittemplate ~/.gittemplate
-ln -s ~/github.com/moutend/dotfiles/.profile ~/.profile
-ln -s ~/github.com/moutend/dotfiles/.vimrc ~/.vimrc
-ln -s ~/github.com/moutend/dotfiles/.zshrc ~/.zshrc
+create_symbolic_link() {
+  if [ ! -f "$HOME/github.com/moutend/dotfiles/$1" ]; then
+    echo "SKIP	create_symbolic_link $1"
+    return 0
+  fi
+  if [ -e "$HOME/$1" ]; then
+    echo "FOUND	create_symbolic_link $1"
+  else
+    ln -s "$HOME/github.com/moutend/dotfiles/$1" "$HOME/$1"
+    echo "OK	create_symbolic_link $1"
+  fi
+}
 
-mkdir ~/.vimswap
+create_say_script() {
+  say -v '?' | grep -q 'kyoko (enhanced)'
+
+  if [ $? -eq 0 ]; then
+    cp "$home/github.com/moutend/dotfiles/bin/say_kyoko_enhanced.bash" "$home/bin/say.bash"
+  else
+    cp "$home/github.com/moutend/dotfiles/bin/say_kyoko.bash" "$home/bin/say.bash"
+  fi
+
+  echo "OK	create_say_script"
+}
+
+set_beep_sound() {
+  if [ -d "$HOME/github.com/moutend/Sounds" ]; then
+    defaults write .GlobalPreferences com.apple.sound.beep.sound "$HOME/github.com/moutend/Sounds/Glass.aiff"
+    echo "OK	set_beep_sound"
+  else
+    echo "SKIP	set_beep_sound"
+  fi
+}
+
+set_beep_sound
+
+create_home_bin
+create_say_script
+
+create_symbolic_link '.profile'
+create_symbolic_link '.zshrc'
+create_symbolic_link '.vimrc'
+create_symbolic_link '.c2d'
+create_symbolic_link '.gitignore'
+create_symbolic_link '.gitconfig'
+create_symbolic_link '.gitconfig.abby-develop'
+
+rm -rf "$HOME/.vimswap" && mkdir "$HOME/.vimswap"
