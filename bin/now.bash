5ae95f0267f8e3ca0e02e477dfd8eb040582f123
Yoshiyuki Koyanagi <moutend@gmail.com>
2023-06-10 09:07:15 +0900

    Update

--- bin/now.bash
+++ bin/now.bash
@@ -4,7 +4,7 @@ hour=`date +%-H`
 minute=`date +%-M`
 
 if [ ${minute} = '0' ]; then
-  $HOME/bin/say.bash -r 210 "${hour}時です。" &
+  $HOME/bin/say.bash -r 210 "${hour}時です。"
 else
-  $HOME/bin/say.bash -r 210 "${hour}時、${minute}分です。" &
+  $HOME/bin/say.bash -r 210 "${hour}時、${minute}分です。"
 fi
