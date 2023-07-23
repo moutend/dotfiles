fcdbe42579ff45e7a95259aaa78fe56651579ac4
Yoshiyuki Koyanagi <moutend@gmail.com>
2023-07-23 15:45:25 +0900

    Update

--- bin/install-monterey.bash
+++ bin/install-monterey.bash
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sudo /Applications/Install\ macOS\ Monterey.app/Contents/Resources/createinstallmedia --volume /Volumes/MyVolume
+# macOS の起動可能なインストーラを作成する
+# https://support.apple.com/ja-jp/HT201372
+# 
+# macOS をダウンロードしてインストールする方法
+# https://support.apple.com/ja-jp/HT211683
+
+sudo /Applications/Install\ macOS\ Monterey.app/Contents/Resources/createinstallmedia --volume /Volumes/USB
