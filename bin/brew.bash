c3513d8562c37557123c3208b6de948ecf1ee7f6
Yoshiyuki Koyanagi <moutend@gmail.com>
2023-12-01 18:33:50 +0900

    Update

--- bin/brew.bash
+++ bin/brew.bash
@@ -5,18 +5,18 @@ set -e
 brew install sox
 brew install direnv
 brew install curl
-# brew install nkf
-# brew install jq
-# brew install pandoc
-# brew install gsed
-# brew install gawk
-# brew install findutils
-# brew install tree
-# brew install exiftool
-# brew install ffmpeg
-# brew install git-lfs
-# brew install youtube-dl
-# brew install blackhole-2ch
+brew install nkf
+brew install jq
+brew install pandoc
+brew install gsed
+brew install gawk
+brew install findutils
+brew install tree
+brew install exiftool
+brew install ffmpeg
+brew install git-lfs
+brew install youtube-dl
+brew install blackhole-2ch
 brew install golangci-lint
 
 brew tap laurent22/massren
